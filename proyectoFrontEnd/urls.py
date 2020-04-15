9ac9d1029100dc6971022137d21809d4abcff1d8
KevinHerrera <kaherrerag@eafit.edu.co>
2020-04-14 20:51:40 -0500

    taller2

--- proyectoFrontEnd/urls.py
+++ proyectoFrontEnd/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from temphum import views as temphum_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', temphum_views.temphum, name='temphum'),
 ]
